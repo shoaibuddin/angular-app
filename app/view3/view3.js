c3472a11b201f8663899d3941c51ee370f12d6f4
Shoaib Ud-Din <suddin@cars.com>
2017-03-20 20:48:03 -0500

    Retrive JSON data

---
+++ app/view3/view3.js
@@ -0,0 +1,39 @@
+'use strict';
+
+angular.module('myApp.view3', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view3', {
+    templateUrl: 'view3/view3.html',
+    controller: 'View3Ctrl'
+  });
+}])
+
+.controller('View3Ctrl', function($scope) {
+
+   $scope.driversList = [
+      {
+          Driver: {
+              givenName: 'Sebastian',
+              familyName: 'Vettel'
+          },
+          points: 322,
+          nationality: "German",
+          Constructors: [
+              {name: "Red Bull"}
+          ]
+      },
+      {
+          Driver: {
+          givenName: 'Fernando',
+              familyName: 'Alonso'
+          },
+          points: 207,
+          nationality: "Spanish",
+          Constructors: [
+              {name: "Ferrari"}
+          ]
+      }
+    ];
+
+});
